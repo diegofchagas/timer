9aad35d02c8520079d5cd810279816e1bf6a9f39
Diego Chagas <diegofchagas@gmail.com>
2024-02-01 19:24:19 -0300

    adicionando theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default.ts'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeProvider theme={defaultTheme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
