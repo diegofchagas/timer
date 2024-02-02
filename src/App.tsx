05fc26f1f5e25586236b5dc800bd5b459c49a98f
Diego Chagas <diegofchagas@gmail.com>
2024-02-02 16:28:08 -0300

    alterando o header com as informacoes corretas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 function App() {
-  
   return (
-    <>
-    oi
-    </>
-  )
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import { Scroll, Timer } from "@phosphor-icons/react"
+import { HeaderContainer } from "./styles"
+import { NavLink } from "react-router-dom"
+
+
+export const Header = () => {
+  return (
+   <HeaderContainer>
+    TIMER
+    <nav>
+     <NavLink to="/" title="Timer"><Timer size={28}/></NavLink>  
+     <NavLink to="/history" title="Histórico"> <Scroll size={28}/></NavLink>
+    </nav>
+    </HeaderContainer>
+  )
+}
