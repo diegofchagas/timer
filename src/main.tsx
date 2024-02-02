cb7a7dbf9c814cdd11efaf3560650a8cf401f5bf
Diego Chagas <diegofchagas@gmail.com>
2024-02-02 13:02:35 -0300

    adicionando font family  e estilo globais

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default.ts'
+import { GlobalStyle } from './styles/global.ts'
 
 
 
@@ -10,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
     <App />
+    <GlobalStyle/>
     </ThemeProvider>
   </React.StrictMode>,
 )
