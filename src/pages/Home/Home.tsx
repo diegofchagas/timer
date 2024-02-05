7393de3372e477ab5de53fe7bd88a0d8d2c48486
Diego Chagas <diegofchagas@gmail.com>
2024-02-05 16:17:16 -0300

    adicionando lib zod para validacao dos dados de formulario e tambem a tipagem dos mesmos

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,46 @@
 import { Play } from "@phosphor-icons/react";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartButton, TaskInput } from "./styles";
+import {useForm} from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe o nome da tarefa'),
+  minutesAmount:zod.number().min(5).max(60)
+})
+
+// como estou utilizando o zod para validação não precisar criar uma interface para validar os tipos que estou recenbendo com a ajuda do zod eu posso inferir os dados , no caso essa inferencia vira minha iterface assim utilizando tudo que a biblioteca nós proporcionar para a validação dos dados.           
+//"Sempre que utilizamos variaves javascript no typescript precisamos informa ao typescript o tipo da mesma para não haver erros de identificação de tipo de variável, por isso uso de typeof".
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export const Home = () => {
+  const {register, handleSubmit,watch,reset} = useForm<NewCycleFormData>({
+    resolver:zodResolver(newCycleFormValidationSchema),
+    defaultValues:{
+      task:'',
+      minutesAmount:0
+    }
+  });
+
+  const handleCreateNewCicle=(data: NewCycleFormData)=>{
+    console.log(data)
+    //Esse reset restarta meus dados para o valor default dos inputs.
+    reset();
+  }
+
+  const task = watch('task');
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCicle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em </label>
           <TaskInput 
           id="task"
           list="task-suggestions"
-          placeholder="Dê um nome para o seu projeto" 
+          placeholder="Dê um nome para o seu projeto"
+          {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -28,6 +58,7 @@ export const Home = () => {
           step={5}
           min={5}
           max={60}
+          {...register('minutesAmount', {valueAsNumber:true})}
           />
           <span>minutos.</span>
         </FormContainer>
@@ -39,7 +70,7 @@ export const Home = () => {
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
-        <StartButton  type="submit">
+        <StartButton disabled={isSubmitDisabled}  type="submit">
           <Play size={24} />
           Começar
         </StartButton>
