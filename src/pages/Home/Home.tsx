03998f627cd68ab38eac5337998bfe8ade7ecf9b
Diego Chagas <diegofchagas@gmail.com>
2024-02-06 15:44:46 -0300

    adicionando funcionalidades ao inicio e fim de ciclo para cada projeto criado e estilizacoes dos buttons de start e stop

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,34 @@
-import { Play } from "@phosphor-icons/react";
-import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartButton, TaskInput } from "./styles";
+import { HandPalm, Play } from "@phosphor-icons/react";
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartButton, TaskInput,StopButton } from "./styles";
 import {useForm} from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from "date-fns";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe o nome da tarefa'),
-  minutesAmount:zod.number().min(5).max(60)
+  minutesAmount:zod.number().min(1).max(60)
 })
 
 // como estou utilizando o zod para validação não precisar criar uma interface para validar os tipos que estou recenbendo com a ajuda do zod eu posso inferir os dados , no caso essa inferencia vira minha iterface assim utilizando tudo que a biblioteca nós proporcionar para a validação dos dados.           
 //"Sempre que utilizamos variaves javascript no typescript precisamos informa ao typescript o tipo da mesma para não haver erros de identificação de tipo de variável, por isso uso de typeof".
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle{
+  id:string
+  task:string
+  minutesAmount: number,
+  startDate: Date,
+  interruptedDate?: Date
+  finishedDate?: Date
+}
+
 export const Home = () => {
+  const [cycles, setCycles]= useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId]= useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
   const {register, handleSubmit,watch,reset} = useForm<NewCycleFormData>({
     resolver:zodResolver(newCycleFormValidationSchema),
     defaultValues:{
@@ -21,15 +36,86 @@ export const Home = () => {
       minutesAmount:0
     }
   });
+    const activeTaskId = cycles.find(task => task.id === activeCycleId)
+    const totalSeconds = activeTaskId ? activeTaskId.minutesAmount * 60 : 0
+
+  useEffect(()=>{
+    let interval: number;
+
+    if(activeTaskId){
+      interval = setInterval(()=>{
+       const secondsDifference = differenceInSeconds(new Date(), activeTaskId.startDate)
+
+        if(secondsDifference >= totalSeconds){
+          setCycles(state => state.map((cycle)=>{
+            if(cycle.id === activeCycleId){
+              return {...cycle, finishedDate: new Date()}
+            } else {
+              return cycle
+            }
+          })
+        )
+        setAmountSecondsPassed(totalSeconds)
+        clearInterval(interval)
+        } else{
+          setAmountSecondsPassed(secondsDifference)
+        }
+      },1000)
+    }
+
+    return ()=>{
+      clearInterval(interval)
+    }
+  },[activeCycleId, activeTaskId, totalSeconds])
 
   const handleCreateNewCicle=(data: NewCycleFormData)=>{
-    console.log(data)
+    const newCycle={
+      id: String(new Date().getTime()),
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date()
+    }
+    setCycles((state)=>[...state, newCycle])
+    setActiveCycleId(newCycle.id)
+    setAmountSecondsPassed(0)
+
     //Esse reset restarta meus dados para o valor default dos inputs.
     reset();
   }
 
-  const task = watch('task');
-  const isSubmitDisabled = !task
+
+  const handleInterruptCycle = ()=> {
+        setCycles(state => state.map((cycle)=>{
+          if(cycle.id === activeCycleId){
+            return {...cycle, interruptedDate: new Date()}
+          } else {
+            return cycle
+          }
+        })
+          )
+        setActiveCycleId(null)
+  }
+    
+
+    
+    const currentSeconds = activeTaskId ? totalSeconds - amountSecondsPassed : 0
+
+    const minutesAmount = Math.floor(currentSeconds / 60);
+    const secondsAmount = currentSeconds % 60;
+
+    const minutes = minutesAmount.toString().padStart(2 ,'0')
+    const seconds = secondsAmount.toString().padStart(2 ,'0')
+
+    const task = watch('task');
+    const isSubmitDisabled = !task
+
+    useEffect(()=>{
+      if(activeTaskId){
+        document.title = `${minutes} ${seconds}`
+      }
+    },[minutes,seconds,activeTaskId])
+
+    console.log(cycles)
 
   return (
     <HomeContainer>
@@ -40,6 +126,7 @@ export const Home = () => {
           id="task"
           list="task-suggestions"
           placeholder="Dê um nome para o seu projeto"
+          disabled={!!activeTaskId}
           {...register('task')}
           />
 
@@ -56,24 +143,30 @@ export const Home = () => {
           id="minutesAmount" 
           placeholder="00"
           step={5}
-          min={5}
+          min={1}
           max={60}
+          disabled={!!activeTaskId}
           {...register('minutesAmount', {valueAsNumber:true})}
           />
           <span>minutos.</span>
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
-        <StartButton disabled={isSubmitDisabled}  type="submit">
+        { activeTaskId ?
+          <StopButton onClick={handleInterruptCycle} type='button'>
+            <HandPalm size={24}/>
+            Interromper
+          </StopButton>
+        : <StartButton disabled={isSubmitDisabled}  type="submit">
           <Play size={24} />
           Começar
-        </StartButton>
+        </StartButton>}
       </form>
     </HomeContainer>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,112 +1,121 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const HomeContainer = styled.main`
   flex: 1;
 
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-  justify-content:center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-  form{
-    display:flex;
-    align-items:center;
-    flex-direction:column;
-    gap:3.5rem;
+  form {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 3.5rem;
   }
 `;
 
 export const FormContainer = styled.div`
-  width:100%;
-  display:flex;
-  align-items:center;
-  justify-content:center;
-  gap:0.5rem;
-  color:${({theme})=> theme['gray-100']};
-  font-size:1.125rem;
-  flex-wrap:wrap;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  color: ${({ theme }) => theme["gray-100"]};
+  font-size: 1.125rem;
+  flex-wrap: wrap;
 `;
 
 export const BaseInput = styled.input`
-  background:transparent;
-  height:2.5rem;
-  border:0;
-  border-bottom: 2px solid ${({theme})=> theme['gray-500']};
-  font-weight:bold;
-  font-size:1.125rem;
-  padding:0 0.5rem;
-  color:${({theme})=> theme['gray-100']};
-  outline:none;
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${({ theme }) => theme["gray-500"]};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${({ theme }) => theme["gray-100"]};
+  outline: none;
 
-  &:focus{
-    border-color:${({theme})=> theme['green-500']};
+  &:focus {
+    border-color: ${({ theme }) => theme["green-500"]};
   }
 
-  &::placeholder{
-    color:${({theme})=> theme['gray-500']};
+  &::placeholder {
+    color: ${({ theme }) => theme["gray-500"]};
   }
-`
+`;
 export const TaskInput = styled(BaseInput)`
-  flex:1;
+  flex: 1;
 
-  &::-webkit-calendar-picker-indicator{
-    display:none !important;
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
   }
-`
+`;
 
 export const MinutesAmountInput = styled(BaseInput)`
-  width:4rem;
-`
+  width: 4rem;
+`;
 
 export const CountdownContainer = styled.div`
-  font-family: 'Roboto Mono', monospace;
-  font-size:10rem;
-  line-height:8rem;
-  color: ${({theme})=> theme['gray-100']};
+  font-family: "Roboto Mono", monospace;
+  font-size: 10rem;
+  line-height: 8rem;
+  color: ${({ theme }) => theme["gray-100"]};
 
-  display:flex;
-  gap:1rem;
+  display: flex;
+  gap: 1rem;
 
-  span{
-    background-color:${({theme})=> theme['gray-700']};
-    padding:2rem 1rem;
-    border-radius:8px;
+  span {
+    background-color: ${({ theme }) => theme["gray-700"]};
+    padding: 2rem 1rem;
+    border-radius: 8px;
   }
 `;
 
 export const Separator = styled.div`
-  padding:2rem 0;
-  color:${({theme})=> theme['green-500']};
+  padding: 2rem 0;
+  color: ${({ theme }) => theme["green-500"]};
 
-  width:4rem;
-  display:flex;
-  overflow:hidden;
-  justify-content:center;
-`
+  width: 4rem;
+  display: flex;
+  overflow: hidden;
+  justify-content: center;
+`;
 
-export const StartButton = styled.button`
-  width:100%;
-  border:0;
-  padding:1rem;
-  border-radius:8px;
+export const BaseButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
 
-  display:flex;
-  align-items:center;
-  justify-content:center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-  gap:0.5rem;
-  font-weight:bold;
+  gap: 0.5rem;
+  font-weight: bold;
   cursor: pointer;
+  color: ${({ theme }) => theme["gray-100"]};
 
-  background:${({theme})=> theme['green-500']};
-  color:${({theme})=> theme['gray-100']};
-
-  &:disabled{
-    opacity:0.7;
-    cursor:not-allowed;
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
   }
+`;
 
-  &:not(:disabled):hover{
-    background:${({theme})=> theme['green-700']}; 
+export const StartButton = styled(BaseButton)`
+  background: ${({ theme }) => theme["green-500"]};
+
+  &:not(:disabled):hover {
+    background: ${({ theme }) => theme["green-700"]};
   }
-`
+`;
+export const StopButton = styled(BaseButton)`
+  background: ${({ theme }) => theme["red-500"]};
+
+  &:not(:disabled):hover {
+    background: ${({ theme }) => theme["red-700"]};
+  }
+`;
