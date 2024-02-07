284103e3f208ac07e1c14bbcb17e28b35315b8bc
Diego Chagas <diegofchagas@gmail.com>
2024-02-07 15:11:18 -0300

    separando os componentes da home em componentes unicos e adicionando o uso de contextApi nos mesmos

---
+++ src/pages/Home/components/Countdown/index.tsx
@@ -0,0 +1,60 @@
+import { useContext,useEffect } from "react";
+import { CountdownContainer,Separator } from "./style";
+import { differenceInSeconds } from "date-fns";
+import { CyclesContext } from "../..";
+
+export const CountDown = () => {
+  const {activeTaskId,activeCycleId,markCurrentCycleAsFinished,amountSecondsPassed,setSecondsPassed } = useContext(CyclesContext);
+  
+
+  const totalSeconds = activeTaskId ? activeTaskId.minutesAmount * 60 : 0;
+
+  useEffect(() => {
+    let interval: number;
+
+    if (activeTaskId) {
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeTaskId.startDate
+        );
+
+        if (secondsDifference >= totalSeconds) {
+          markCurrentCycleAsFinished()
+          setSecondsPassed(totalSeconds);
+          clearInterval(interval);
+        } else {
+          setSecondsPassed(secondsDifference);
+        }
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeCycleId, activeTaskId, totalSeconds,markCurrentCycleAsFinished,setSecondsPassed]);
+
+  const currentSeconds = activeTaskId ? totalSeconds - amountSecondsPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+  const minutes = minutesAmount.toString().padStart(2, "0");
+  const seconds = secondsAmount.toString().padStart(2, "0");
+
+  useEffect(() => {
+    if (activeTaskId) {
+      document.title = `${minutes} ${seconds}`;
+    }
+  }, [minutes, seconds, activeTaskId]);
+
+  return (
+    <CountdownContainer>
+      <span>{minutes[0]}</span>
+      <span>{minutes[1]}</span>
+      <Separator>:</Separator>
+      <span>{seconds[0]}</span>
+      <span>{seconds[1]}</span>
+    </CountdownContainer>
+  );
+};
---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,44 @@
+
+import { useContext } from 'react'
+import {FormContainer,TaskInput,MinutesAmountInput} from './styles'
+import { CyclesContext } from '../..'
+import { useFormContext } from 'react-hook-form'
+
+export const NewCycleForm = () => {
+
+  const {activeTaskId} = useContext(CyclesContext)
+  const {register} = useFormContext()
+  
+  return (
+    <FormContainer>
+    <label htmlFor="task">Vou trabalhar em </label>
+    <TaskInput 
+    id="task"
+    list="task-suggestions"
+    placeholder="Dê um nome para o seu projeto"
+    disabled={!!activeTaskId}
+    {...register('task')}
+    />
+
+    <datalist id="task-suggestions">
+      <option value="projeto 1"/>
+      <option value="projeto 2"/>
+      <option value="projeto 3"/>
+      <option value="projeto 4"/>
+    </datalist>
+
+    <label htmlFor="minutesAmount">durante</label>
+    <MinutesAmountInput 
+    type="number" 
+    id="minutesAmount" 
+    placeholder="00"
+    step={5}
+    min={1}
+    max={60}
+    disabled={!!activeTaskId}
+    {...register('minutesAmount', {valueAsNumber:true})}
+    />
+    <span>minutos.</span>
+  </FormContainer>
+  )
+}
